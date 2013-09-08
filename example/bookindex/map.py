7bdba07d00cd5575becd322c1e5ecb27c98eb530
Adam Monsen <haircut@gmail.com>
2013-09-07 22:23:41 -0700

    bookindex map.py: improve variable name

--- example/bookindex/map.py
+++ example/bookindex/map.py
@@ -4,7 +4,7 @@ for line in sys.stdin:
     page_name, text = line.split(':')
     text = text.rstrip()
     page_number = page_name.split('-')[1]
-    matches = re.findall('\w+', text)
-    for word in matches:
+    words = re.findall('\w+', text)
+    for word in words:
         if len(word) < 4: continue
         print('{}\t{}'.format(word.lower(), page_number))
