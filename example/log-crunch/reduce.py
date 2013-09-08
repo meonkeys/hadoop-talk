23604794a6614dbc63303ce5048b2823b423ff55
Adam Monsen <haircut@gmail.com>
2013-09-07 21:56:20 -0700

    reduce.py: add documentation (comments)

--- example/log-crunch/reduce.py
+++ example/log-crunch/reduce.py
@@ -3,15 +3,15 @@ import sys
 last_host = None
 last_count = 0
 host = None
-for line in sys.stdin:
+for line in sys.stdin: # pre-sorted by Hadoop or our shell pipeline
     host, count = line.split('\t')
     count = int(count)
-    if last_host == host:
+    if last_host == host: # add more to sum for this host
         last_count += count
-    else:
-        if last_host:
+    else: # new host -- print last host & count, reset both
+        if last_host: # don't print first line
             print '%s\t%s' % (last_host, last_count)
         last_host = host
         last_count = count
-if last_host == host:
+if last_host == host: # last line different (we just reset stuff), print
     print '%s\t%s' % (last_host, last_count)
